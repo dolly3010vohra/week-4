870b5256f45e349b99038a411236bcc96095fc95
Dolly Vohra <dolly.vohra3010@gmail.com>
2024-11-08 15:58:22 +0000

    week4

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+result = math.sin(6.2)
+from math import sqrt
+root = sqrt(49)
+from math import *
+root = sqrt(49)
+val = cos(radians(45))
+
+from math impost pi
+print("The value"of pi is", pi)
+def findMax(a,b) :
+    ***Finds the maximum of two values.***
+if ( a > b ) :
+    max = a
+else:
+    max = b
+    return max
